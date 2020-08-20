135a669b57b2f516a5a93090462a99fd36b57c81
Iain Diamond <iain.diamond@gmail.com>
2020-08-20 12:15:42 +0100

    make data private

--- src/tictactoe/utils.cljs
+++ src/tictactoe/utils.cljs
@@ -24,20 +24,21 @@
 (defn- moves-left? [game]
   (some #(= :empty %) game))
 
+(def ^:private winning [[0 1 2] ; rows
+                        [3 4 5]
+                        [6 7 8]
+                        [0 3 6] ; columns
+                        [1 4 7]
+                        [2 5 8]
+                        [0 4 8] ; diagonals
+                        [2 4 6]])
+
 (defn check-game [game]
-  (let [winning [[0 1 2] ; rows
-                 [3 4 5]
-                 [6 7 8]
-                 [0 3 6] ; columns
-                 [1 4 7]
-                 [2 5 8]
-                 [0 4 8] ; diagonals
-                 [2 4 6]]]
-    (loop [index 0]
-      (let [line (get winning index)
-            result (check-line line game)]
-        (if result
-          result
-          (if (< index (count winning))
-            (recur (inc index))
-            (if (moves-left? game) nil :draw)))))))
+  (loop [index 0]
+    (let [line (get winning index)
+          result (check-line line game)]
+      (if result
+        result
+        (if (< index (count winning))
+          (recur (inc index))
+          (if (moves-left? game) nil :draw))))))
